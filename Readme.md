591957128790ca0ebd30aafdbf2191ed43e6d40d
Fabrizio Tappero <fabrizio.tappero@gmail.com>
2023-03-17 15:10:35 +0000

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+Simple cmake boilerplate for C++ project
+
+cd cmake_bp
+build.sh
+
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BUILD_DIR=build
+
+rm -r ${BUILD_DIR} 2> /dev/null
+mkdir -p ${BUILD_DIR}
+
+pushd ${BUILD_DIR}
+    #conan install .. --build missing   # install dependecies in conanofile.txt
+    cmake ..                            # gen build system
+    cmake --build .                     # build all targets
+    #ctest --verbose                    # run all tests
+popd
+
