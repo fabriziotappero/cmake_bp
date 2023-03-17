bf4fbf5b3a9a409a6a39415e5925655155509c6a
Fabrizio Tappero <fabrizio.tappero@gmail.com>
2023-03-17 15:15:16 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -r ${BUILD_DIR} 2> /dev/null
 mkdir -p ${BUILD_DIR}
 
 pushd ${BUILD_DIR}
-    #conan install .. --build missing   # install dependecies in conanofile.txt
+    #conan install .. --build missing   # install dependecies defined in conanfile.txt
     cmake ..                            # gen build system
     cmake --build .                     # build all targets
     #ctest --verbose                    # run all tests
